45baa07aa2deaad330196b71ab68c4b739927204
Ian Balaguera <balagueraian@gmail.com>
2019-03-08 09:18:50 -0500

    Create basic demoblock texture

--- src/main/java/com/rlapcs/radiotransfer/client.java
+++
@@ -1,4 +0,0 @@
-package com.rlapcs.radiotransfer;
-
-public class client {
-}
--- src/main/java/com/rlapcs/radiotransfer/common/DemoBlock.java
+++ src/main/java/com/rlapcs/radiotransfer/common/DemoBlock.java
@@ -3,11 +3,22 @@ package com.rlapcs.radiotransfer.common;
 import com.rlapcs.radiotransfer.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DemoBlock extends Block {
     public DemoBlock() {
         super(Material.GRASS);
         setUnlocalizedName(RadioTransfer.MODID + ".demoblock");
         setRegistryName("demoblock");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0,
+                new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
