41d37d3d7628a396f08b5f7ca9006ce00a6426a8
Ian Richardson <ian.irrtech@gmail.com>
2019-03-08 10:01:49 -0500

    Add Basic Recipe

--- src/main/java/com/rlapcs/radiotransfer/common/items/DemoItem.java
+++ src/main/java/com/rlapcs/radiotransfer/common/items/DemoItem.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Item;
 
 public class DemoItem extends Item {
     public DemoItem() {
-        setRegistryName("demoitem");        // The unique name (within your mod) that identifies this item
+        setRegistryName("demoitem");  // The unique name (within your mod) that identifies this item
         setUnlocalizedName(RadioTransfer.MODID + ".demoitem");     // Used for localization (en_US.lang)
     }
 
