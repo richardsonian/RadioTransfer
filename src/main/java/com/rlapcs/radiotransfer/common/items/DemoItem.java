6e5a8e307de2168efed5461db2e738a4118aceba
Ian Richardson <ian.irrtech@gmail.com>
2019-03-11 21:18:16 -0400

    Add DemoItem texture and clean up Registries

--- src/main/java/com/rlapcs/radiotransfer/common/items/DemoItem.java
+++ src/main/java/com/rlapcs/radiotransfer/common/items/DemoItem.java
@@ -1,12 +1,21 @@
 package com.rlapcs.radiotransfer.common.items;
 
 import com.rlapcs.radiotransfer.RadioTransfer;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DemoItem extends Item {
     public DemoItem() {
         setRegistryName("demoitem");  // The unique name (within your mod) that identifies this item
         setUnlocalizedName(RadioTransfer.MODID + ".demoitem");     // Used for localization (en_US.lang)
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 
 }
--- src/main/java/com/rlapcs/radiotransfer/common/items/ModItems.java
+++ src/main/java/com/rlapcs/radiotransfer/common/items/ModItems.java
@@ -1,9 +1,16 @@
 package com.rlapcs.radiotransfer.common.items;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModItems {
 
     @GameRegistry.ObjectHolder("radiotransfer:demoitem")
     public static DemoItem demoItem;
+
+    @SideOnly(Side.CLIENT)
+    public static void initModels() {
+        demoItem.initModel();
+    }
 }
