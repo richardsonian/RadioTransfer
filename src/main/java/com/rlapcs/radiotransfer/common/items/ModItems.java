416552c24aa1d17d083baecda383a48ed6ff945a
Ian Richardson <ian.irrtech@gmail.com>
2019-03-13 14:03:56 -0400

    Tweak ModItems

--- src/main/java/com/rlapcs/radiotransfer/common/items/ModItems.java
+++ src/main/java/com/rlapcs/radiotransfer/common/items/ModItems.java
@@ -3,21 +3,23 @@ package com.rlapcs.radiotransfer.common.items;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+@ObjectHolder("radiotransfer")
 public class ModItems {
 
-    @GameRegistry.ObjectHolder("radiotransfer:demoitem")
-    public static DemoItem demoItem;
+    @ObjectHolder("demoitem")
+    public static final DemoItem demoItem = null;
 
-    @GameRegistry.ObjectHolder("radiotransfer:redgem")
-    public static Item redGem;
+    @ObjectHolder("redgem")
+    public static final Item redGem = null;
 
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         demoItem.initModel();
+        //redgem
         ModelLoader.setCustomModelResourceLocation(redGem, 0, new ModelResourceLocation(redGem.getRegistryName(), "inventory"));
     }
 }
