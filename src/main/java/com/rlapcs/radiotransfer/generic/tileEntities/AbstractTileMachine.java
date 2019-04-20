f897f583eee628998c5f0cbb35164ff50adf1ca9
Ian Richardson <ian.irrtech@gmail.com>
2019-04-20 12:02:07 -0400

    TileEntity data persistence
    (still have not implemented stateless gui updating)

--- src/main/java/com/rlapcs/radiotransfer/generic/tileEntities/AbstractTileMachine.java
+++ src/main/java/com/rlapcs/radiotransfer/generic/tileEntities/AbstractTileMachine.java
@@ -2,11 +2,15 @@ package com.rlapcs.radiotransfer.generic.tileEntities;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nullable;
 
 public abstract class AbstractTileMachine extends TileEntity {
     public static final double MINIMUM_DISTANCE_TO_USE = 64D;
@@ -38,6 +42,48 @@ public abstract class AbstractTileMachine extends TileEntity {
         super.writeToNBT(compound);
         compound.setTag("items", itemStackHandler.serializeNBT());
         return compound;
+    }
+
+    // When the world loads from disk, the server needs to send the TileEntity information to the client
+    //  it uses getUpdatePacket(), getUpdateTag(), onDataPacket(), and handleUpdateTag() to do this:
+    //  getUpdatePacket() and onDataPacket() are used for one-at-a-time TileEntity updates
+    //  getUpdateTag() and handleUpdateTag() are used by vanilla to collate together into a single chunk update packet
+    //  Not really required for this example since we only use the timer on the client, but included anyway for illustration
+    @Override
+    @Nullable
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        NBTTagCompound nbtTagCompound = new NBTTagCompound();
+        writeToNBT(nbtTagCompound);
+
+        int metadata = getBlockMetadata();
+        return new SPacketUpdateTileEntity(this.pos, metadata, nbtTagCompound);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        readFromNBT(pkt.getNbtCompound());
+        //do something with block metadata?
+    }
+
+    /*
+    Creates a tag containing the TileEntity information, used by vanilla to transmit from server to client
+     */
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        NBTTagCompound nbtTagCompound = new NBTTagCompound();
+        writeToNBT(nbtTagCompound);
+        return nbtTagCompound;
+    }
+
+    /*
+    Populates this TileEntity with information from the tag, used by vanilla to transmit from server to client
+     */
+    @Override
+    public void handleUpdateTag(NBTTagCompound tag)
+    {
+        this.readFromNBT(tag);
     }
 
     public boolean canInteractWith(EntityPlayer playerIn) {
