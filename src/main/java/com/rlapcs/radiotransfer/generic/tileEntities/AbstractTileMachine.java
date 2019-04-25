1f0aac2da9e12ab861a5c1d90969de0340547848
Ian Richardson <ian.irrtech@gmail.com>
2019-04-25 12:02:03 -0400

    Add AbstractTileMachineWithInventory

--- src/main/java/com/rlapcs/radiotransfer/generic/tileEntities/AbstractTileMachine.java
+++ src/main/java/com/rlapcs/radiotransfer/generic/tileEntities/AbstractTileMachine.java
@@ -1,54 +1,35 @@
 package com.rlapcs.radiotransfer.generic.tileEntities;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
 
 public abstract class AbstractTileMachine extends TileEntity implements ITickable {
     public static final double MINIMUM_DISTANCE_TO_USE = 64D;
 
-    protected ItemStackHandler itemStackHandler;
     protected long ticksSinceCreation;
 
-    public AbstractTileMachine(int itemStackHandlerSize) {
+    public AbstractTileMachine() {
         super();
 
         ticksSinceCreation = 0;
-
-        itemStackHandler = new ItemStackHandler(itemStackHandlerSize) {
-            @Override
-            protected void onContentsChanged(int slot) {
-                // We need to tell the tile entity that something has changed so
-                // that the chest contents is persisted
-                AbstractTileMachine.this.markDirty();
-            }
-        };
     }
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
-        if (compound.hasKey("items")) {
-            itemStackHandler.deserializeNBT((NBTTagCompound) compound.getTag("items"));
-        }
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
-        compound.setTag("items", itemStackHandler.serializeNBT());
         return compound;
     }
 
@@ -101,48 +82,12 @@ public abstract class AbstractTileMachine extends TileEntity implements ITickabl
 
     @Override
     public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return true;
-        }
         return super.hasCapability(capability, facing);
     }
 
     @Override
     public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(itemStackHandler);
-        }
         return super.getCapability(capability, facing);
-    }
-
-    /**
-     * Attempts to merge a stack into an ItemHandler, and returns what was not able to be merged.
-     * Returns ItemStack.EMPTY if the merge was 100% successful
-     * @param stack
-     * @return The remainder of the stack
-     */
-    public ItemStack mergeStackIntoInventory(ItemStack stack) {
-        if(!world.isRemote) {
-            if(stack.isEmpty() || stack == null) return ItemStack.EMPTY;
-
-            ItemStack itemstack = stack.copy();
-            int slot = 0;
-            while(!itemstack.isEmpty() && slot < this.itemStackHandler.getSlots()) {
-                itemstack = this.itemStackHandler.insertItem(slot, itemstack, false);
-                slot++;
-            }
-
-            //No need for block update
-            /*
-            if(ItemStack.areItemStacksEqual(stack, itemstack)) { //if something was merged into the inventory
-                //world.notifyBlockUpdate(getPos(), world.getBlockState(getPos()), world.getBlockState(getPos()), 2);
-            }
-            */
-
-            return itemstack;
-        }
-
-        return null;
     }
 
     @Override
---
+++ src/main/java/com/rlapcs/radiotransfer/generic/tileEntities/AbstractTileMachineWithInventory.java
@@ -0,0 +1,93 @@
+package com.rlapcs.radiotransfer.generic.tileEntities;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nullable;
+
+public abstract class AbstractTileMachineWithInventory extends AbstractTileMachine {
+    protected ItemStackHandler itemStackHandler;
+
+    public AbstractTileMachineWithInventory(int itemStackHandlerSize) {
+        super();
+
+        ticksSinceCreation = 0;
+
+        itemStackHandler = new ItemStackHandler(itemStackHandlerSize) {
+            @Override
+            protected void onContentsChanged(int slot) {
+                // We need to tell the tile entity that something has changed so
+                // that the chest contents is persisted
+                AbstractTileMachineWithInventory.this.markDirty();
+            }
+        };
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        if (compound.hasKey("items")) {
+            itemStackHandler.deserializeNBT((NBTTagCompound) compound.getTag("items"));
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        compound.setTag("items", itemStackHandler.serializeNBT());
+        return compound;
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return true;
+        }
+        return super.hasCapability(capability, facing);
+    }
+
+    @Override
+    public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(itemStackHandler);
+        }
+        return super.getCapability(capability, facing);
+    }
+
+    /**
+     * Attempts to merge a stack into an ItemHandler, and returns what was not able to be merged.
+     * Returns ItemStack.EMPTY if the merge was 100% successful
+     * @param stack
+     * @return The remainder of the stack
+     */
+    public ItemStack mergeStackIntoInventory(ItemStack stack) {
+        if(!world.isRemote) {
+            if(stack.isEmpty() || stack == null) return ItemStack.EMPTY;
+
+            ItemStack itemstack = stack.copy();
+            int slot = 0;
+            while(!itemstack.isEmpty() && slot < this.itemStackHandler.getSlots()) {
+                itemstack = this.itemStackHandler.insertItem(slot, itemstack, false);
+                slot++;
+            }
+
+            //No need for block update
+            /*
+            if(ItemStack.areItemStacksEqual(stack, itemstack)) { //if something was merged into the inventory
+                //world.notifyBlockUpdate(getPos(), world.getBlockState(getPos()), world.getBlockState(getPos()), 2);
+            }
+            */
+
+            return itemstack;
+        }
+
+        return null;
+    }
+}
