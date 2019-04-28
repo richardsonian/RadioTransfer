e22da4e4053c4103334cbd201524aa0528f90300
Ian Richardson <ian.irrtech@gmail.com>
2019-04-27 23:34:03 -0400

    Cache progress on deregister if multiblock chain broken

--- src/main/java/com/rlapcs/radiotransfer/generic/multiblock/tileEntities/AbstractTileMultiblockNode.java
+++ src/main/java/com/rlapcs/radiotransfer/generic/multiblock/tileEntities/AbstractTileMultiblockNode.java
@@ -5,6 +5,10 @@ import com.rlapcs.radiotransfer.generic.tileEntities.AbstractTileMachine;
 import com.rlapcs.radiotransfer.machines.radio.TileRadio;
 import com.sun.org.apache.xpath.internal.operations.Mult;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.rlapcs.radiotransfer.RadioTransfer.sendDebugMessage;
 
@@ -30,6 +34,8 @@ public abstract class AbstractTileMultiblockNode extends AbstractTileMachine {
             controller.removeNode(this);
             this.controller = null;
             registeredInMultiblock = false;
+
+            this.notifySurroundingDetatch();
         }
     }
     public boolean isRegisteredInMultiblock() {
@@ -44,6 +50,66 @@ public abstract class AbstractTileMultiblockNode extends AbstractTileMachine {
                 this.deregisterFromMultiblock();
             }
         }
+    }
+
+    private boolean isAdjacentToController() {
+        BlockPos[] neighbors = {pos.up(), pos.down(), pos.north(), pos.south(), pos.east(), pos.west()};
+        for(BlockPos n : neighbors) {
+            TileEntity te = world.getTileEntity(n);
+            if(te instanceof TileRadio) {
+                if(((TileRadio) te).getMultiblockController() == controller) {
+                    sendDebugMessage(this + " is adjacent to " + controller);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private List<AbstractTileMultiblockNode> getNeighboringNodes() {
+        List<AbstractTileMultiblockNode> nodes = new ArrayList<>();
+
+        BlockPos[] neighbors = {pos.up(), pos.down(), pos.north(), pos.south(), pos.east(), pos.west()};
+        for(BlockPos n : neighbors) {
+            TileEntity te = world.getTileEntity(n);
+            if(te instanceof AbstractTileMultiblockNode) {
+                sendDebugMessage("found neighboring multiblock tile " + te + (registeredInMultiblock ? "(registered)" : "(deregistered)")+ " with controller" + ((AbstractTileMultiblockNode) te).getController());
+                if(((AbstractTileMultiblockNode) te).isRegisteredInMultiblock() && ((AbstractTileMultiblockNode) te).getController() == this.getController()) {
+                    sendDebugMessage("adding " + te + "to neighbor list");
+                    nodes.add((AbstractTileMultiblockNode) te);
+                }
+            }
+        }
+        sendDebugMessage("found neighboring nodes: " + nodes);
+        return nodes;
+    }
+    private boolean isConnectedToController() {
+        if(this.isAdjacentToController()) return true;
+        else {
+            for(AbstractTileMultiblockNode node : getNeighboringNodes()) {
+                if(node.isConnectedToController()) {
+                    sendDebugMessage(this + " is connected to its controller");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    private void notifySurroundingDetatch() {
+        for(AbstractTileMultiblockNode node : getNeighboringNodes()) {
+            sendDebugMessage(this + " notified detatch to " + node);
+            verifyConnectedToController();
+        }
+    }
+    private void verifyConnectedToController() {
+        if(registeredInMultiblock) {
+            if(!isConnectedToController()) {
+                this.deregisterFromMultiblock();
+            }
+        }
+    }
+
+    public MultiblockRadioController getController() {
+        return controller;
     }
 
     public abstract double getPowerUsagePerTick();
